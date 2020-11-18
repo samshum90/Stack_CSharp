19face747ff098384b9a54a4d34b8121a9a048a7
Samuel Shum <55974596+samshum90@users.noreply.github.com>
2020-11-18 01:14:24 +0000

    Finished

--- Stack_CSharp/Program.cs
+++ Stack_CSharp/Program.cs
@@ -6,8 +6,20 @@ namespace Stack_CSharp
     {
         static void Main(string[] args)
         {
-           
+
+            Stack stack = new Stack();
+
+            int ini = 1;
+            int max = 5;
+
+            for (int i = ini; i <= max; i++)
+                stack.Push(i);
             
+            for (int i = ini; i <= max; i++)
+                Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
         }
     }
+
 }
---
+++ Stack_CSharp/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_CSharp
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You cannot add a null object.");
+
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("There are no objects in the stack yet.");
+
+            int index = _list.Count - 1;
+
+            object toReturn = _list[index];
+
+            _list.RemoveAt(index);
+
+            return (toReturn);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+
+}
